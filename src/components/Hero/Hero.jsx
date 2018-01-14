fd99cfefa5b53740e2e1c298d98e10df4538c6db
Ben Newton <bennewton999@gmail.com>
2018-01-13 23:44:53 -0500

    change Hero to functional stateless component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,25 +5,21 @@ import config from "../../../data/SiteConfig";
 import UserLinks from "../UserLinks/UserLinks";
 import "./Hero.scss";
 
-class Hero extends Component {
-  render() {
-    return (
-      <div className="hero">
-        <Link to="/about">
-          <Overdrive id="me">
-            <img
-              src={config.userAvatar}
-              className="me"
-              alt={config.userName}
-            />
-          </Overdrive>
-        </Link>
-        <h1 className="my-name">Ben Newton</h1>
-        <h2>Full Stack JavaScript Developer at <a href="http://sapientrazorfish.com/">SR<span className="sr">_</span></a></h2>
-        <UserLinks contactMe config={config} />
-      </div>
-    );
-  }
-}
+const Hero = () => (
+  <div className="hero">
+    <Link to="/about">
+      <Overdrive id="me">
+        <img
+          src={config.userAvatar}
+          className="me"
+          alt={config.userName}
+        />
+      </Overdrive>
+    </Link>
+    <h1 className="my-name">Ben Newton</h1>
+    <h2>Full Stack JavaScript Developer at <a href="http://sapientrazorfish.com/">SR<span className="sr">_</span></a></h2>
+    <UserLinks contactMe config={config} />
+  </div>
+);
 
 export default Hero;
