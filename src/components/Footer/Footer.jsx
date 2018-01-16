30e988869ab0c4939862e22c9b20ba8481378337
Ben Newton <bennewton999@gmail.com>
2018-01-15 21:36:16 -0500

    make footer functional component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,33 +5,31 @@ import UserLinks from "../UserLinks/UserLinks";
 import config from "../../../data/SiteConfig";
 import "./Footer.scss";
 
-class Footer extends Component {
-  render() {
-    const url = config.siteRss;
-    const { userLinks } = this.props;
-    const copyright = config.copyright;
-    if (!copyright) {
-      return null;
-    }
-    return (
-      <footer className="footer">
-        {userLinks ? <UserLinks config={config} labeled /> : null}
-        <div className="notice-container">
-          <div className="copyright">
-            <h4>
-              {copyright}
-            </h4>
-          </div>
-
-          <div className="rss">
-            <Link to={url}>
-              <Button flat secondary iconClassName="fa fa-rss" />
-            </Link>
-          </div>
-        </div>
-      </footer>
-    );
+const Footer = () => {
+  
+  const url = config.siteRss;
+  const { userLinks } = this.props;
+  const copyright = config.copyright;
+  if (!copyright) {
+    return null;
   }
+  return (
+    <footer className="footer">
+      {userLinks ? <UserLinks config={config} labeled /> : null}
+      <div className="notice-container">
+        <div className="copyright">
+          <h4>
+            {copyright}
+          </h4>
+        </div>
+        <div className="rss">
+          <Link to={url}>
+            <Button flat secondary iconClassName="fa fa-rss" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
