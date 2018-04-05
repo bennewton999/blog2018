b8543b5f69d3b57f852509e5152ee9ba7a73b077
Ben Newton <bennewton999@gmail.com>
2018-04-05 19:25:47 -0400

    test cypress change

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,18 +1,18 @@
-describe("Home Page Test", () => {
-  it("Successfully loads", () => {
-    cy.visit("http://localhost:9000/");
-    cy.screenshot();
+// describe("Home Page Test", () => {
+//   it("Successfully loads", () => {
+//     cy.visit("/");
+//     cy.screenshot();
 
-    cy.get(".my-name").should("contain", "Ben Newton");
-  });
-  it("Has correct Title", () => {
-    cy.title().should("eq", "Ben Newton");
-  });
-});
+//     cy.get(".my-name").should("contain", "Ben Newton");
+//   });
+//   it("Has correct Title", () => {
+//     cy.title().should("eq", "Ben Newton");
+//   });
+// });
 
 describe("Uses Page Test", () => {
   it("Successfully loads", () => {
-    cy.visit("http://localhost:9000/uses");
+    cy.visit("/uses");
     cy.screenshot();
 
     cy.get(".my-name").should("contain", "What I Use");
