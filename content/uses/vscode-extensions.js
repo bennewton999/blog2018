0e0265cc907c6fbab3516b63d060ffd7bf970ce8
Ben Newton <bennewton999@gmail.com>
2018-06-15 17:59:07 -0400

    Move Extentions to loop

---
+++ content/uses/vscode-extensions.js
@@ -0,0 +1,59 @@
+const vsCodeExtensions = [
+    {
+        title: 'Code Settings Sync',
+        icon: 'code-settings-sync-icon.png',
+        link: 'https://github.com/shanalikhan/code-settings-sync',
+        description: `Working across 2, sometimes 3 computers, this is a no brainer.  Plus when moving to a new computer, it speeds things up.`,
+    },
+    {
+        title: 'Prettier',
+        icon: 'prettier-icon.png',
+        link: 'https://github.com/wesbos/cobalt2-vscode',
+        description: `If you're still spacing everything out nice and neat on your own, you're wasting time.  This is the best thing to come along in a while.  And make sure you set your editor.formatOnSave to true!`,
+    },
+    {
+        title: 'Cobalt 2 Theme',
+        icon: 'cobalt2-icon.png',
+        link: 'https://github.com/wesbos/cobalt2-vscode',
+        description: `My current theme from Wes Bos.`,
+    },
+    {
+        title: 'Git Lens',
+        icon: 'debugger-for-chrome-icon.png',
+        link: 'http://gitlens.amod.io/',
+        description: `I mainly use Git Lens for the git blame feature, but it has a number of git enhancements for VSCode.`,
+    },
+    {
+        title: 'Rainbow Brackets',
+        icon: 'rainbowBrackets-icon.png',
+        link: 'https://marketplace.visualstudio.com/items?itemName=2gua.rainbow-brackets',
+        description: `Easy traversing of bracketed data in the editor.`,
+    },
+    {
+        title: 'Color Highlight',
+        icon: 'color-highlight-icon.png',
+        link: 'https://github.com/sergiirocks/vscode-ext-color-highlight',
+        description: `Adds colored border around css/web colors in the editor.`,
+    },
+    {
+        title: 'Debugger for Chrome',
+        icon: 'debugger-for-chrome-icon.png',
+        link: '',
+        description: `Debug your JavaScript code running in Google Chrome from VSCode.`,
+    },
+    {
+        title: 'EditorConfig',
+        icon: 'editor-config-icon.png',
+        link: 'https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig',
+        description: `This plugin attempts to override user/workspace settings with settings found in .editorconfig files.`,
+    },
+    {
+        title: 'Eslint',
+        icon: 'eslint-icon.png',
+        link: 'https://github.com/Microsoft/vscode-eslint',
+        description: `The extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the
+        extension looks for a global install version.`,
+    }
+];
+
+export default vsCodeExtensions;
--- src/components/Uses/Uses.jsx
+++ src/components/Uses/Uses.jsx
@@ -14,17 +14,9 @@ import "./Uses.scss";
 
 import vscodeIcon from "../../images/vscode-icon.png";
 import webstormIcon from "../../images/webstorm-icon.svg";
-import prettierIcon from "../../images/prettier-icon.png";
-import cobalt2Icon from "../../images/cobalt2-icon.png";
-import carbonIcon from "../../images/carbon-icon.svg";
-import rainbowBracketsIcon from "../../images/rainbowBrackets-icon.png";
-import gitLensIcon from "../../images/gitlens-icon.png";
-import codeSyncIcon from "../../images/code-settings-sync-icon.png";
 import appleIcon from "../../images/apple-icon.png";
-import colorHighlightIcon from "../../images/color-highlight-icon.png";
-import debuggerForChromeIcon from "../../images/debugger-for-chrome-icon.png";
-import editorConfigIcon from "../../images/editor-config-icon.png";
-import eslintIcon from "../../images/eslint-icon.png";
+
+import vsCodeExtensions from "../../../content/uses/vscode-extensions";
 
 class About extends Component {
   componentDidMount() {
@@ -85,141 +77,23 @@ class About extends Component {
               </List>
               <List>
                 <CardTitle title="Plugins" />
-
-                <a
-                  href="https://github.com/shanalikhan/code-settings-sync"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={codeSyncIcon} />}
-                    primaryText="Code Settings Sync"
-                  />
-                </a>
-                <CardText>
-                  {`Working across 2, sometimes 3 computers, this is a no brainer.  Plus when moving to a new computer, it speeds things up.`}
-                </CardText>
-                <a
-                  href="https://github.com/prettier/prettier-vscode"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={prettierIcon} />}
-                    primaryText="Prettier"
-                  />
-                </a>
-                <CardText>
-                  {`If you're still spacing everything out nice and neat on your own, you're wasting time.  This is the best thing to come along in a while.  And make sure you set your editor.formatOnSave to true!`}
-                </CardText>
-                <a
-                  href="https://github.com/wesbos/cobalt2-vscode"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={cobalt2Icon} />}
-                    primaryText="Cobalt 2 Theme"
-                  />
-                </a>
-                <CardText>My current theme from Wes Bos.</CardText>
-                <a
-                  href="https://marketplace.visualstudio.com/items?itemName=ericadamski.carbon-now-sh"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={carbonIcon} />}
-                    primaryText="Carbon Now sh"
-                  />
-                </a>
-                <CardText>
-                  Opens the current editor content in carbon.now.sh and share
-                  beautiful images of your source code.
-                </CardText>
-                <a
-                  href="http://gitlens.amod.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={gitLensIcon} />}
-                    primaryText="Git Lens"
-                  />
-                </a>
-                <CardText>
-                  I mainly use Git Lens for the git blame feature, but it has a
-                  number of git enhancements for VSCode.
-                </CardText>
-                <a
-                  href="https://marketplace.visualstudio.com/items?itemName=2gua.rainbow-brackets"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={rainbowBracketsIcon} />}
-                    primaryText="Rainbow Brackets"
-                  />
-                </a>
-                <CardText>
-                  Easy traversing of bracketed data in the editor.
-                </CardText>
-                <a
-                  href="https://github.com/sergiirocks/vscode-ext-color-highlight"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={colorHighlightIcon} />}
-                    primaryText="Color Highlight"
-                  />
-                </a>
-                <CardText>
-                  Adds colored border around css/web colors in the editor.
-                </CardText>
-                <a
-                  href="https://github.com/sergiirocks/vscode-ext-color-highlight"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={debuggerForChromeIcon} />}
-                    primaryText="Debugger for Chrome"
-                  />
-                </a>
-                <CardText>
-                  Debug your JavaScript code running in Google Chrome from VS
-                  Code.
-                </CardText>
-                <a
-                  href="https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={editorConfigIcon} />}
-                    primaryText="EditorConfig"
-                  />
-                </a>
-                <CardText>
-                  This plugin attempts to override user/workspace settings with
-                  settings found in .editorconfig files.
-                </CardText>
-                <a
-                  href="https://github.com/Microsoft/vscode-eslint"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItem
-                    leftAvatar={<Avatar src={eslintIcon} />}
-                    primaryText="Eslint"
-                  />
-                </a>
-                <CardText>
-                  {`The extension uses the ESLint library installed in the opened
-                  workspace folder. If the folder doesn't provide one the
-                  extension looks for a global install version.`}
-                </CardText>
+                {vsCodeExtensions.map((ext) => (
+                  <div key={ext.title}>
+                    <a
+                      href={ext.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <ListItem
+                        leftAvatar={<Avatar src={`/vs-ext/${ext.icon}`} />}
+                        primaryText={ext.title}
+                      />
+                    </a>
+                    <CardText>
+                      {ext.description}
+                    </CardText>
+                  </div>
+                ))}
               </List>
 
               <CardTitle title="Hardware" />
