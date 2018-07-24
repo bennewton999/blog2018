5739f3d89f037b5f02c1599a82e8e589c0f276a3
Ben Newton <bennewton999@gmail.com>
2018-07-23 23:30:18 -0400

    Feature: Add gatsby Gist include component
    This component allows a GIST to be included in a gatsby post with
    an embed code.

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,19 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-embed-gist',
+            options: {
+              // Optional:
+
+              // the github handler whose gists are to be accessed
+              // username: 'weirdpattern',
+
+              // a flag indicating whether the github default gist css should be included or not
+              // default: true
+              includeDefaultCss: true
+            }
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               // Class prefix for <pre> tags containing syntax highlighting;
